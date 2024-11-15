94204d031809fa92c286c596bae98a27263ddd3c
Computer Virus <86645415+computer-virus@users.noreply.github.com>
2024-11-14 23:54:30 -0700

    Finalized Comments

--- js/canvas.js
+++ js/canvas.js
@@ -37,8 +37,8 @@ function step() {
 
             // Check all 8 neighboring cells (up, down, left, right, and diagonals)
             // Explanation: This double for-loop have the i and j indexes mapped to relative coords using the current row and column as the origin.
-            // The values i and j will both loop from -1 to 1 [i.e. -1, 0, 1] and these relative coords will be added to the origin's coord to get the modified neighbour coords.
-            // For example, if the origin coord is [1, 2] and the relative coord is [-1, -1], then when the relative coord is applied the final coord will be [0, 1].
+            // The values i and j will both loop from -1 to 1 [i.e. -1, 0, 1] and these relative coords will be added to the origin's coords to get the modified neighbour coords.
+            // For example, if the origin coords are [1, 2] and the relative coords are [-1, -1], then when the relative coords are applied the final coord will be [0, 1].
             // This exact double for-loop is used quite often when looking for neighbours.
             for (let i = -1; i <= 1; i++) {
                 for (let j = -1; j <= 1; j++) {
@@ -49,10 +49,10 @@ function step() {
                     let neighborRow = row + i;
                     let neighborCol = col + j;
 
-                    // Check if the neighbor is within bounds of the grid
+                    // Check if the neighbor is within bounds of the grid*
                     // Remember that the board loops around, meaning if it's out of bounds you need to wrap the position around, back in bounds.
                     // The lines below do not do this...
-                    // You'd be better off checking to see if each coord is out of bounds forst, wrapping it if so, and then finally once both coords as validated increment aliveNeighbours as needed.
+                    // You'd be better off checking to see if each coord is out of bounds first, wrapping it if so, and then finally once both coords are validated increment aliveNeighbours as needed.
                     if (neighborRow >= 0 && neighborRow < gridMaxRows && neighborCol >= 0 && neighborCol < gridMaxColumns) {
                         aliveNeighbors += cloneGrid[neighborRow][neighborCol] ? 1 : 0; //If the neighbor is alive add 1, otherwise add 0.
                     }
@@ -61,14 +61,14 @@ function step() {
 
             // Rule 1 and Rule 3: Any live cell with fewer than 2 or more than 3 live neighbors dies.
             // The === operator takes data types into consideration, so something like 5 === "5" will always be false.
-            // Make sure when you use that operator, you're using the right data type. Otherwise, use the == operator.
+            // Make sure that when you use that operator you're using the right data type. Otherwise, use the == operator which ignores data types.
             if (cloneGrid[row][col] === true) {
                 // This works but you don't need both if statements here.
                 // An alive cell will stay alive if you don't modify it.
                 if (aliveNeighbors < 2 || aliveNeighbors > 3) {
                     array[row][col] = false; // Again, be mindful of data types
                 } // Rule 2: Any live cell with exactly 2 or 3 live neighbors survives
-                // This else-if can go.
+                // This else-if can go. No need to set true to true.
                 else if (aliveNeighbors === 2 || aliveNeighbors === 3) {
                     array[row][col] = true; // Data types
                 }
